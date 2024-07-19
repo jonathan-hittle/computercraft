44203d1b0946cef2e97fdd5c7b77ae007286e702
jwhittle <jonathan.hittle@gmail.com>
2024-07-19 14:54:35 -0600

    maybe fix infinite loop; fix use of local as loop control that goes away after loop

--- elevator.lua
+++ elevator.lua
@@ -6,9 +6,11 @@ function usage()
     print("elevator <up|down>")
 end
  
+got_response=false
+
 rednet.open("right")
 rednet.send(server_id, LV8_REQ..direction, LV8_PROTOCOL)
-for response_waits = 1, 5 do
+for response_waits = 1,5 do
 	local id, message, resp_prot = rednet.receive()
 	sId = message["nSender"]
 	sProt = message["sProtocol"] or "nil"
@@ -20,11 +22,13 @@ for response_waits = 1, 5 do
 		print("from comp id, protocol: "..id..", "..sProt)
 	end
 	if id == server_id and resp_prot == LV8_PROTOCOL and lv8_mess_is_response(sMess) then
+		got_response=true
 		break
 	end
 	sleep(5)
 end
-if response_waits == 5 then
+
+if not got_response then
 	print("Timed out waiting for response")
 	print("Engage optical scanners to confirm current status.")
 end
