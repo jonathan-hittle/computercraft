29f849679c47d0b54263db8fcc7eb368f6762e2d
jwhittle <jonathan.hittle@gmail.com>
2024-07-21 14:26:41 -0600

    differentiate between pocket and regular computer modem direction; add string response handling to client

--- elevator.lua
+++ elevator.lua
@@ -5,18 +5,30 @@ local server_id = 20 -- magic no. ; server comp id
 function usage()
     print("elevator <up|down>")
 end
+
+if pocket then
+	modem_direction = "back"
+else
+	modem_direction = "right"
+end
  
 got_response=false
 
-rednet.open("right")
+rednet.open(modem_direction)
 rednet.send(server_id, LV8_REQ..direction, LV8_PROTOCOL)
 for response_waits = 1,5 do
 	print("Attempt "..response_waits.." to wait for response")
 	local id, message, resp_prot = rednet.receive(5)
 	if id ~= nil then
-		sId = message["nSender"]
-		sProt = message["sProtocol"] or "nil"
-		sMess = message["message"]
+		if type(message) == "table" then
+			sId = message["nSender"]
+			sProt = message["sProtocol"] or "nil"
+			sMess = message["message"]
+		else
+			sId = id
+			sProt = resp_prot or "nil"
+			sMess = message
+		end
 		if id == server_id and resp_prot == LV8_PROTOCOL and lv8_mess_is_response(sMess) then
 			print("received response: "..sMess)
 		else
@@ -36,4 +48,4 @@ if not got_response then
 	print("Timed out waiting for response")
 	print("Engage optical scanners to confirm current status.")
 end
-rednet.close("right")
+rednet.close(modem_direction)
